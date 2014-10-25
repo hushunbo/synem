14b9110e9cb443f4906f612c7728de1982e6fa86
Omar Ocegueda <jomaroceguedag@gmail.com>
2014-10-24 19:44:38 -0500

    Added code to generate the graphs

---
+++ experiments/plot_series.py
@@ -0,0 +1,31 @@
+#graph_title = 'Jaccard index of 31 anatomical regions averaged over 612 multi-modal registrations'
+graph_title = 'Jaccard index of 31 anatomical regions averaged over 306 mono-modal registrations'
+data_fname = 'data_mono_seg.txt'
+regions_fname = 'xlabels_mono_seg.txt'
+series_fname = 'series_mono_seg.txt'
+
+
+data = np.loadtxt(data_fname)
+nrows, ncols = data.shape
+
+with open(regions_fname) as input:
+    xlabels = [s for s in input.readlines()]
+    
+with open(series_fname) as input:
+    series = input.readlines()
+
+markers = ['o','D','s','^']
+linestyles = ['--', '--', '--', '--']
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+for s in range(ncols):
+    line, = ax.plot(range(1, nrows+1), data[:,s], linestyle=linestyles[s], marker=markers[s])
+    line.set_label(series[s])
+ax.legend()
+plt.xticks(np.array(range(1,32)), xlabels, rotation='vertical', horizontalalignment='left')
+plt.grid()
+plt.ylim(0, 1)
+plt.tight_layout()
+plt.title(graph_title, fontsize=32)
+plt.ylabel('Jaccard index', fontsize=24)
