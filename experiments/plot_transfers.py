b8f9f9c6b0f9b4ee5fb776891663a38e0d90ba40
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-01-04 17:45:07 -0600

    Add boxplots

---
+++ experiments/plot_transfers.py
@@ -0,0 +1,4 @@
+m2, v2 = get_mean_transfer(t2, t1)
+draw_boxplots(m2, v2)
+graph_title = "Estimated transfer function between T2-T1 modalities from the Brainweb template"
+plt.title(graph_title, fontsize=32)
